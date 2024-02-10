631b2442d940fe93fc662440aff449fae6abfde1
Fateme Kalantari <fatemezahrakalantarii@gmail.com>
2024-02-10 13:56:14 +0330

    modify error_handler.py and add unauthorized() handler

--- app/middleware.py
+++ app/middleware.py
@@ -5,17 +5,11 @@ from jose import JWTError
 from app.utils.token_parser import token_parser
 from sqlalchemy.orm import Session
 from app.database import get_db
-# from app.controllers.user import get_by_id
-# from app.controllers.lawyer import get_by_id
+# from app.controllers.user import UserController
+# from app.controllers.lawyer import LawyerController
 
 
 class CustomMiddleware(BaseHTTPMiddleware):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={os.getenv("AUTH_HEADER_NAME"): "Bearer"},
-    )
-
     async def dispatch(self, request: Request, call_next, db: Session = Depends(get_db)):
         print("CustomMiddleware applied.")
         if request.url.path.startswith("/specific-api"):
@@ -27,14 +21,14 @@ class CustomMiddleware(BaseHTTPMiddleware):
         return response
 
     async def user_authenticate(self, request: Request, db):
-        try:
-            user_id = token_parser(request)
-            user = await get_user_by_id(db, user_id)
-            if not user:
-                raise self.credentials_exception
-        except:
-            print("Request handler error--------------------------")
-            raise self.credentials_exception
+        # try:
+        #     user_id = token_parser(request)
+        #     user = await get_user_by_id(db, user_id)
+        #     if not user:
+        #         raise self.credentials_exception
+        # except:
+        #     print("Request handler error--------------------------")
+        #     raise self.credentials_exception
         print("User Authentication middleware applied")
 
     async def lawyer_authenticate(self, request: Request, db):
--- app/utils/error_handler.py
+++ app/utils/error_handler.py
@@ -1,15 +1,35 @@
+import os
+from fastapi import status
+
+
 class CustomException(Exception):
-    def __init__(self, status_code: int, detail: str):
+    def __init__(self, status_code: int, message: str):
         self.status_code = status_code
-        self.detail = detail
+        self.detail = {
+            "status_code": status_code,
+            "error_message": message
+        }
 
 
 class ErrorHandler:
     @staticmethod
     def not_found(item: str):
-        raise CustomException(status_code=404, detail=f"{item} Not Found")
+        raise CustomException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            message=f"{item} Not Found"
+        )
+
+    @staticmethod
+    def unauthorized():
+        header_name = os.getenv("AUTH_HEADER_NAME")
+        raise CustomException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            message=f"Could Not Validate Credentials ({header_name} Header)"
+        )
 
     @staticmethod
     def internal_server_error():
         raise CustomException(
-            status_code=500, detail="Internal Server Error")
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            message="Internal Server Error"
+        )
