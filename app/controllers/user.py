39e49c7c1092f39e7d07938b6683efee7668446e
Fateme Kalantari <fatemezahrakalantarii@gmail.com>
2024-02-10 13:01:05 +0330

    user controllers get_all and get_by_id

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -1,59 +1,57 @@
 from sqlalchemy.orm import Session
 from app.database import SessionLocal, User
 
-# Function to get all users
+
+class UserController:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_all(self):
+        return self.db.query(User).all()
+
+    def create(self, username: str, email: str):
+        new_user = User(username=username, email=email)
+        self.db.add(new_user)
+        self.db.commit()
+        self.db.refresh(new_user)
+        return new_user
+
+    def get_by_id(self, user_id: int):
+        return self.db.query(User).filter(User.id == user_id).first()
+
+    def update(self, user_id: int, username: str):
+        user = self.db.query(User).filter(User.id == user_id).first()
+        user.username = username
+        self.db.commit()
+        self.db.refresh(user)
+        return user
+
+    def delete(self, user_id: int):
+        user = self.db.query(User).filter(User.id == user_id).first()
+        self.db.delete(user)
+        self.db.commit()
 
 
-def get_users(db: Session):
-    return db.query(User).all()
+# # Example of usage
+# if __name__ == "__main__":
+#     # Create a session
+#     db = SessionLocal()
 
+#     # Create a new user
+#     new_user = create_user(db, username="john_doe", email="john@example.com")
+#     print("Created User:", new_user)
 
-def create_user(db: Session, username: str, email: str):
-    new_user = User(username=username, email=email)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+#     # Retrieve all users
+#     users = get_users(db)
+#     print("All Users:", users)
 
+#     # Update a user
+#     updated_user = update_user(db, user_id=new_user.id, username="jane_doe")
+#     print("Updated User:", updated_user)
 
-def get_user_by_id(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
+#     # Delete a user
+#     delete_user(db, user_id=new_user.id)
+#     print("User Deleted")
 
-
-def update_user(db: Session, user_id: int, username: str):
-    user = db.query(User).filter(User.id == user_id).first()
-    user.username = username
-    db.commit()
-    db.refresh(user)
-    return user
-
-
-def delete_user(db: Session, user_id: int):
-    user = db.query(User).filter(User.id == user_id).first()
-    db.delete(user)
-    db.commit()
-
-
-# Example of usage
-if __name__ == "__main__":
-    # Create a session
-    db = SessionLocal()
-
-    # Create a new user
-    new_user = create_user(db, username="john_doe", email="john@example.com")
-    print("Created User:", new_user)
-
-    # Retrieve all users
-    users = get_users(db)
-    print("All Users:", users)
-
-    # Update a user
-    updated_user = update_user(db, user_id=new_user.id, username="jane_doe")
-    print("Updated User:", updated_user)
-
-    # Delete a user
-    delete_user(db, user_id=new_user.id)
-    print("User Deleted")
-
-    # Close the session
-    db.close()
+#     # Close the session
+#     db.close()
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,22 +1,24 @@
 from fastapi import APIRouter, Depends, Path
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.controllers.user import get_users, get_user_by_id
+from app.controllers.user import UserController
 
 router = APIRouter()
 
 
 @router.get("/all")
 async def get_users_route(db: Session = Depends(get_db)):
-    users = get_users(db)
+    user_controller = UserController(db)
+    users = user_controller.get_all()
     db.close()
     return users
 
 
 @router.get("/{user_id}")
-async def get_users_route(
+async def get_user_by_id_route(
         db: Session = Depends(get_db),
         user_id: int = Path(description="This is ID of user to return")):
-    user = get_user_by_id(db, user_id)
+    user_controller = UserController(db)
+    user = user_controller.get_by_id(user_id)
     db.close()
     return user
